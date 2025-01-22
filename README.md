e52057f76218dad661dbadef84c0d871c3557afd
David Dick <ddick@cpan.org>
2025-01-23 06:36:20 +1100

    Updating to 0.52

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Crypt::URandom - Provide non blocking randomness
 
 # VERSION
 
-This document describes Crypt::URandom version 0.51\_02
+This document describes Crypt::URandom version 0.52
 
 # SYNOPSIS
 
--- lib/Crypt/URandom.pm
+++ lib/Crypt/URandom.pm
@@ -17,7 +17,7 @@ our %EXPORT_TAGS = ( 'all' => \@EXPORT_OK, );
 our @CARP_NOT = ('Crypt::URandom');
 
 BEGIN {
-    our $VERSION = '0.51';
+    our $VERSION = '0.52';
     eval {
         require XSLoader;
 
@@ -246,7 +246,7 @@ Crypt::URandom - Provide non blocking randomness
 
 =head1 VERSION
 
-This document describes Crypt::URandom version 0.51
+This document describes Crypt::URandom version 0.52
 
 
 =head1 SYNOPSIS
